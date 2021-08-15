74a290ef19921a6f1e80ae7fabaf3a8f2897984f
NguyenVanToi <toinv@ql6625.fr>
2021-08-15 09:25:13 +0700

    init project

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -23,40 +23,16 @@ interface AppPage {
 
 const appPages: AppPage[] = [
   {
-    title: 'Inbox',
-    url: '/page/Inbox',
+    title: 'Chi tiêu',
+    url: '/page/accountant',
     iosIcon: mailOutline,
     mdIcon: mailSharp
   },
   {
-    title: 'Outbox',
-    url: '/page/Outbox',
+    title: 'Quản lý danh mục',
+    url: '/page/category',
     iosIcon: paperPlaneOutline,
     mdIcon: paperPlaneSharp
-  },
-  {
-    title: 'Favorites',
-    url: '/page/Favorites',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp
-  },
-  {
-    title: 'Archived',
-    url: '/page/Archived',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp
-  },
-  {
-    title: 'Trash',
-    url: '/page/Trash',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp
-  },
-  {
-    title: 'Spam',
-    url: '/page/Spam',
-    iosIcon: warningOutline,
-    mdIcon: warningSharp
   }
 ];
 
@@ -69,8 +45,8 @@ const Menu: React.FC = () => {
     <IonMenu contentId="main" type="overlay">
       <IonContent>
         <IonList id="inbox-list">
-          <IonListHeader>Inbox</IonListHeader>
-          <IonNote>hi@ionicframework.com</IonNote>
+          <IonListHeader>Vinamilk</IonListHeader>
+          <IonNote>toinv2610@gmail.com</IonNote>
           {appPages.map((appPage, index) => {
             return (
               <IonMenuToggle key={index} autoHide={false}>
@@ -84,7 +60,7 @@ const Menu: React.FC = () => {
         </IonList>
 
         <IonList id="labels-list">
-          <IonListHeader>Labels</IonListHeader>
+          <IonListHeader>Danh mục</IonListHeader>
           {labels.map((label, index) => (
             <IonItem lines="none" key={index}>
               <IonIcon slot="start" icon={bookmarkOutline} />
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,11 +1,21 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
 
 const Page: React.FC = () => {
 
-  const { name } = useParams<{ name: string; }>();
+  let { name } = useParams<{ name: string; }>();
+  switch (name) {
+      case 'accountant':
+          name = 'Chi tiêu';
+          break;
+      case 'category':
+          name = 'Quản lý danh mục';
+          break;
+      default:
+          name = 'page';
+          break;
+  }
 
   return (
     <IonPage>
@@ -24,7 +34,6 @@ const Page: React.FC = () => {
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name={name} />
       </IonContent>
     </IonPage>
   );
